20cd082d723b58947f184536ac00b927332f1373
Arina M <apelsinka7.ru@yandex.ru>
2022-10-01 20:41:36 +0300

    решение задачи 25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+
+int GetNumberA()
+{
+    Console.WriteLine("Введите число A: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetNumberB()
+{
+
+    Console.WriteLine("Введите число B: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetCycle(int numberone, int numbertwo)
+{
+    int cycle = 1;
+    for(int i=1; i <= numbertwo; i++)
+    cycle = cycle * numberone;
+    return cycle;
+}
+
+int numberone = GetNumberA();
+int numbertwo = GetNumberB();
+Console.WriteLine($"Число {numberone} в степени {numbertwo} = " + GetCycle(numberone, numbertwo));
