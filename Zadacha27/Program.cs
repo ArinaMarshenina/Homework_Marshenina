93260c2dfbd5add45287c5f16e479ccabfa720ef
Arina M <apelsinka7.ru@yandex.ru>
2022-10-01 21:09:52 +0300

    решение задачи 27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе
+
+int GetNumber()
+{
+    
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSum(int numberA)
+{
+int count = numberA;
+int length = 0;
+int sum = 0;
+
+for (int i = 0; i < count; i++){
+    length = numberA - numberA % 10;
+    sum = sum + (numberA - length);
+    numberA = numberA / 10;
+}
+   return sum;
+}
+int numberA = GetNumber();
+Console.WriteLine($"Сумма цифр в числе {"numberA"}: " + GetSum(numberA));
